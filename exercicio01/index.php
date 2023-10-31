57b989137d352158a6b11989d3d7c876a1384e83
sthefanny-lipu <129000142+sthefanny-lipu@users.noreply.github.com>
2023-10-31 14:20:48 -0400

    Add files via upload

---
+++ exercicio01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>me livrando da maldição</title>
+</head>
+
+<body>
+    <h1>
+
+        <?php
+        echo "olá,mundo!\u{1F30F}"
+        ?>
+    </h1>
+    <p>vamos nos tentar nos livra da maldição </p>
+</body>
+
+</html>
---
+++ exercicio03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    <h1>exemplo de PHP</h1>
+    <?php
+    echo "hoje é dia" . date("d/m/y");
+    echo "e a hora atual é" . date("G:i:s")
+    ?>
+</body>
+
+</html>
